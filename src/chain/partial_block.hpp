80a5df960e6a72ee37f380b99ab9a82b8c7259d1
Junsuk Park <park64kr63@gmail.com>
2018-11-14 16:58:23 +0900

    ADD - PartialBlock 데이터 구조 생성(임시블럭)
    https://thevaulters.atlassian.net/wiki/spaces/SGN/pages/47186101/Enterprise+Data+Modeling

---
+++ src/chain/partial_block.hpp
@@ -0,0 +1,15 @@
+#ifndef GRUUT_HANA_MERGER_TEMPORARY_BLOCK_HPP
+#define GRUUT_HANA_MERGER_TEMPORARY_BLOCK_HPP
+
+#include "types.hpp"
+
+namespace gruut {
+    struct PartialBlock {
+        timestamp sent_time;
+        sender_id_type sender_id;
+        chain_id_type chain_id;
+        block_height_type height;
+        transaction_root_type transaction_root;
+    };
+}
+#endif
--- src/chain/types.hpp
+++ src/chain/types.hpp
@@ -15,6 +15,12 @@ namespace gruut {
     using transaction_id_type = sha256;
     using timestamp = std::string;
     using requestor_id_type = sha256;
+    using sender_id_type = sha256;
+    using transaction_root_type = sha256;
+    using TransactionType = int;
+    using chain_id_type = sha256;
+    using block_height_type = std::string;
+
     using certification_array_type = vector<std::string>;
 }
 
