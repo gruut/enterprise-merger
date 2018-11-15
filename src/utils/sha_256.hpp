b1880cf305dd1fca01a8f830b10d42ab19f5ad22
Junsuk Park <park64kr63@gmail.com>
2018-11-15 13:42:18 +0900

    - Crypto++ Sha256 을 사용하기 위해서는 복잡한 과정이 필요하다. 그래서 쉽게 사용가능한 wrapper class Sha256을 생성함 - modules, utility의 테스트를 구분해야할 필요가 있어서 디렉토리를 나누고 CMakeList 파일 생성
    - Sha256#encrypt
      * 메시지를 받고 그것을 해싱해주는 함수
    - #isMatch
      * encrypt된 문자와 평문을 비교해서 결과를 리턴해주는 함수

---
+++ src/utils/sha_256.hpp
@@ -0,0 +1,42 @@
+#ifndef GRUUT_HANA_MERGER_SHA_256_HPP
+#define GRUUT_HANA_MERGER_SHA_256_HPP
+
+#include <cryptopp/sha.h>
+#include <cryptopp/hex.h>
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Sha256 {
+    using byte = CryptoPP::byte;
+
+public:
+    string encrypt(const string &message) {
+        CryptoPP::SHA256 hash;
+        byte digest[CryptoPP::SHA256::DIGESTSIZE];
+        hash.CalculateDigest(digest, (byte *) message.c_str(), message.length());
+
+        CryptoPP::HexEncoder encoder;
+        string result;
+        encoder.Attach(new CryptoPP::StringSink(result));
+        encoder.Put(digest, sizeof(digest));
+        encoder.MessageEnd();
+
+        std::cout << result << std::endl;
+
+        return result;
+    }
+
+    bool isMatch(const string &target_message, const string &encrypted_message) {
+        auto encrypted_target_message = this->encrypt(target_message);
+        bool result = encrypted_target_message == encrypted_message;
+
+        return result;
+    }
+
+private:
+};
+
+#endif
