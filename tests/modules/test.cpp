b31b8dbc3ebe2db9a7da4bfd1dac2f25ebc9a83f
Junsuk Park <park64kr63@gmail.com>
2018-11-24 09:06:29 +0900

    REMOVE - exclude unnecessary codes

--- tests/modules/test.cpp
+++ tests/modules/test.cpp
@@ -141,13 +141,6 @@ BOOST_AUTO_TEST_SUITE_END()
 BOOST_AUTO_TEST_SUITE(Test_Storage)
 
     BOOST_AUTO_TEST_CASE(write_block) {
-        string tx_list;
-        for (unsigned int idx = 0; idx < block["tx"].size() - 1; ++idx) {
-            tx_list += block["tx"][idx]["txID"];
-            tx_list += '_';
-        }
-        block["block"]["txList"] = tx_list;
-
         Storage storage;
         storage.openDB("./block");
 
