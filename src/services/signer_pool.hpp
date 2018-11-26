5ebfd8adb357d19c43dfeea22588d0e800cf5d8f
Junsuk Park <park64kr63@gmail.com>
2018-11-26 16:14:40 +0900

    REFACTOR - SignerPool class 로 분리
    ## 수정사항
    - SignerPool을 하나의 class로 분리해달라는 요청에 따라 클래스 생성 후 분리
    - Signer 데이터를 넣거나 가져오는 일들을 모두 SignerPool 에게 delegate하도록 수정

---
+++ src/services/signer_pool.hpp
@@ -0,0 +1,26 @@
+#ifndef GRUUT_ENTERPRISE_MERGER_SIGNER_POOL_HPP
+#define GRUUT_ENTERPRISE_MERGER_SIGNER_POOL_HPP
+
+#include "../chain/signer.hpp"
+#include <cstddef>
+#include <vector>
+
+using namespace std;
+
+namespace gruut {
+using Signers = vector<Signer>;
+
+class SignerPool {
+public:
+  size_t size();
+
+  Signer get(unsigned int index);
+  Signers fetchAll();
+  void insert(Signer &&signer);
+
+private:
+  Signers m_signers;
+};
+} // namespace gruut
+
+#endif
