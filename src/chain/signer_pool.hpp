0a42ac946038ccb7b22e42e02c4eb9bf6b2b003b
Jeonil Kang <dreamx@thevaulters.com>
2018-11-27 15:59:19 +0900

    FIXED: tried to attemp mutex locking for updating data

--- src/chain/signer_pool.hpp
+++ src/chain/signer_pool.hpp
@@ -7,11 +7,17 @@
 
 #include <iostream>
 #include <string>
-#include <vector>
+#include <deque>
 #include <array>
 #include <ctime>
 
+#include <thread>
+#include <mutex>
+
+#include <botan/secmem.h>
+
 #include "../utils/template_singleton.hpp"
+
 
 enum SSTATUS {
   UNKNOWN,
@@ -19,24 +25,25 @@ enum SSTATUS {
   LINK_ERROR
 };
 
-struct SignerEntry{
+typedef struct _SignerEntry {
   uint64_t user_id{0};
   std::string pk_cert;
-  std::vector<uint8_t> hmac_key;
+  Botan::secure_vector<uint8_t> hmac_key;
   std::time_t last_update{0};
   int stat{SSTATUS::UNKNOWN};
-};
+} SignerEntry;
 
 namespace gruut {
 
-class SignerPool : TemplateSingleton<SignerPool>{
+class SignerPool : public TemplateSingleton<SignerPool>{
 
 private:
-  std::vector<SignerEntry> m_signer_pool;
-
+  std::deque<SignerEntry> m_signer_pool;
+  std::mutex m_push_mutex;
+  std::mutex m_update_mutex;
 public:
 
-  void pushSigner(uint64_t user_id, std::string &pk_cert, std::vector<uint8_t> &hmac_key, int stat = SSTATUS::UNKNOWN) {
+  void pushSigner(uint64_t user_id, std::string &pk_cert, Botan::secure_vector<uint8_t> &hmac_key, int stat = SSTATUS::UNKNOWN) {
 
     SignerEntry tmp_entry;
     tmp_entry.user_id = user_id;
@@ -49,13 +56,20 @@ public:
     for(SignerEntry &entry : m_signer_pool) {
       if(entry.user_id == user_id) {
         is_in = true;
+
+        std::lock_guard<std::mutex> guard(m_push_mutex);
         entry = tmp_entry;
+        m_push_mutex.unlock();
+
         break;
       }
     }
 
-    if(!is_in)
+    if(!is_in) {
+      std::lock_guard<std::mutex> guard(m_push_mutex);
       m_signer_pool.emplace_back(tmp_entry);
+      m_push_mutex.unlock();
+    }
 
   }
 
@@ -64,8 +78,12 @@ public:
     for(SignerEntry &entry : m_signer_pool) {
       if(entry.user_id == user_id) {
         is_in = true;
+
+        std::lock_guard<std::mutex> guard(m_update_mutex);
         entry.pk_cert = pk_cert;
         entry.last_update = std::time(nullptr);
+        m_update_mutex.unlock();
+
         break;
       }
     }
@@ -73,13 +91,17 @@ public:
     return is_in;
   }
 
-  bool updateHmacKey(uint64_t user_id, std::vector<uint8_t> &hmac_key){
+  bool updateHmacKey(uint64_t user_id, Botan::secure_vector<uint8_t> &hmac_key){
     bool is_in = false;
     for(SignerEntry &entry : m_signer_pool) {
       if(entry.user_id == user_id) {
         is_in = true;
+
+        std::lock_guard<std::mutex> guard(m_update_mutex);
         entry.hmac_key = hmac_key;
         entry.last_update = std::time(nullptr);
+        m_update_mutex.unlock();
+
         break;
       }
     }
@@ -92,8 +114,12 @@ public:
     for(SignerEntry &entry : m_signer_pool) {
       if(entry.user_id == user_id) {
         is_in = true;
+
+        std::lock_guard<std::mutex> guard(m_update_mutex);
         entry.stat = stat;
         entry.last_update = std::time(nullptr);
+        m_update_mutex.unlock();
+
         break;
       }
     }
@@ -106,7 +132,11 @@ public:
     for(size_t i = 0; i < m_signer_pool.size(); ++i) {
       if(m_signer_pool[i].user_id == user_id) {
         is_in = true;
+
+        std::lock_guard<std::mutex> guard(m_push_mutex);
         m_signer_pool.erase(m_signer_pool.begin() + i);
+        m_push_mutex.unlock();
+
         break;
       }
     }
@@ -114,8 +144,8 @@ public:
     return is_in;
   }
 
-  std::vector<uint8_t> getHmacKey(uint64_t user_id) {
-    std::vector<uint8_t> ret_hmac_key;
+  Botan::secure_vector<uint8_t> getHmacKey(uint64_t user_id) {
+    Botan::secure_vector<uint8_t> ret_hmac_key;
     for(SignerEntry &entry : m_signer_pool) {
       if(entry.user_id == user_id) {
         ret_hmac_key = entry.hmac_key;
@@ -164,6 +194,12 @@ public:
     }
 
     return ret_list;
+  }
+
+  void emptyPool(){
+    std::lock_guard<std::mutex> guard(m_push_mutex);
+    m_signer_pool.clear();
+    m_push_mutex.unlock();
   }
 
   // TODO: May be we should do more operation, but we cannot recognize what operations are required.
