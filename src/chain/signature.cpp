ef0a0dab50f95aa1035793fae5ad2780b6177f6a
davincism <psm@gruut.net>
2018-11-28 14:05:59 +0900

    # ADD-@ - signature.cpp에 주석

--- src/chain/signature.cpp
+++ src/chain/signature.cpp
@@ -1,6 +1,6 @@
 /**
  * @file signature.cpp
- * @brief Signer의 서명 구조체
+ * @brief Signer의 서명 구조체를 정의
  */
 
 #ifndef GRUUT_ENTERPRISE_MERGER_SIGNATURE_HPP
@@ -9,10 +9,14 @@
 #include "types.hpp"
 
 namespace gruut {
+
+/**
+ * @brief Signer의 서명 구조체
+ */
 struct Signature {
-  signer_id_type signer_id;
-  timestamp sent_time;
-  signature_type signer_signature;
+  signer_id_type signer_id;         ///Signer의 아이디
+  timestamp sent_time;              ///송신 시간
+  signature_type signer_signature;  ///Signer의 서명
 };
 } // namespace gruut
 #endif
