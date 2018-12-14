4027ab405c1b98c00d817e8d0081230f2b7286e7
blackiron <jjj@thevaulters.com>
2018-12-14 17:21:41 +0900

    # FIX - Signer의 id가 다르게 오는 문제 임시해결
    -  Signer에서 open channel시 id가 base64로 와서 생긴문제 임시 해결
     * 임시로 decoding 후 사용하도록 함.

--- src/modules/communication/merger_server.cpp
+++ src/modules/communication/merger_server.cpp
@@ -1,6 +1,7 @@
 #include "merger_server.hpp"
 #include "message_handler.hpp"
 #include "rpc_receiver_list.hpp"
+#include "../../utils/type_converter.hpp"
 #include <chrono>
 #include <cstring>
 #include <thread>
@@ -123,6 +124,9 @@ void OpenChannel::proceed() {
     // TODO :: 고칩시다!
     std::string id(sizeof(uint64_t) - m_request.sender().length(), 0x00);
     id += m_request.sender();
+    // uint64_t receiver_id;
+    // std::string encoded_id = m_request.sender();
+    // std::vector<uint8_t> id = TypeConverter::decodeBase64(encoded_id);
     std::memcpy(&receiver_id, &id[0], sizeof(uint64_t));
 
     m_rpc_receiver_list->setReqSsig(receiver_id, &m_stream, this);
